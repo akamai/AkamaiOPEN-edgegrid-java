f57d2e59d0e3c7ccf8d35bf6ab61d4c8befdfbd3
Martin Meyer <mmeyer@akamai.com>
2017-12-15 11:31:38 -0500

    Restore support for port embedded in the hostname

--- edgegrid-signer-apache-http-client/src/main/java/com/akamai/edgegrid/signer/apachehttpclient/ApacheHttpClientEdgeGridRoutePlanner.java
+++ edgegrid-signer-apache-http-client/src/main/java/com/akamai/edgegrid/signer/apachehttpclient/ApacheHttpClientEdgeGridRoutePlanner.java
@@ -32,7 +32,18 @@ public class ApacheHttpClientEdgeGridRoutePlanner extends DefaultRoutePlanner {
     public HttpRoute determineRoute(HttpHost host, HttpRequest request, HttpContext context) throws HttpException {
         try {
             ClientCredential clientCredential = binding.getClientCredentialProvider().getClientCredential(binding.map(request));
-            HttpHost target = new HttpHost(clientCredential.getHost(), -1, "https");
+            String hostname = clientCredential.getHost();
+            int port = -1;
+            final int pos = hostname.lastIndexOf(":");
+            if (pos > 0) {
+                try {
+                    port = Integer.parseInt(hostname.substring(pos + 1));
+                } catch (NumberFormatException ex) {
+                    throw new IllegalArgumentException("Host contains invalid port number: " + hostname);
+                }
+                hostname = hostname.substring(0, pos);
+            }
+            HttpHost target = new HttpHost(hostname, port, "https");
             return super.determineRoute(target, request, context);
         } catch (NoMatchingCredentialException e) {
             throw new RuntimeException(e);
