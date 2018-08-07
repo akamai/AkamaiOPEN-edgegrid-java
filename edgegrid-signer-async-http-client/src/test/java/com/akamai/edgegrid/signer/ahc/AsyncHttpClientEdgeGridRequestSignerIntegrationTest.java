17eadd0e9ce05778cd891a525828fa9085d46fc3
Martin Meyer <mmeyer@akamai.com>
2018-08-07 11:20:46 -0400

    Fix copyright declarations on new files

--- edgegrid-signer-async-http-client/src/test/java/com/akamai/edgegrid/signer/ahc/AsyncHttpClientEdgeGridRequestSignerIntegrationTest.java
+++ edgegrid-signer-async-http-client/src/test/java/com/akamai/edgegrid/signer/ahc/AsyncHttpClientEdgeGridRequestSignerIntegrationTest.java
@@ -1,12 +1,26 @@
+/*
+ * Copyright 2018 Akamai Technologies, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.akamai.edgegrid.signer.ahc;
 
 import com.akamai.edgegrid.signer.ClientCredential;
 import com.github.tomakehurst.wiremock.WireMockServer;
 
-import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.Request;
 import org.asynchttpclient.RequestBuilder;
-import org.asynchttpclient.Response;
 import org.hamcrest.CoreMatchers;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -19,7 +33,6 @@ import java.util.concurrent.ExecutionException;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.matching;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
--- edgegrid-signer-gatling/src/test/scala/com/akamai/edgegrid/signer/gatling/TestData.scala
+++ edgegrid-signer-gatling/src/test/scala/com/akamai/edgegrid/signer/gatling/TestData.scala
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 package com.akamai.edgegrid.signer.gatling
 
 import com.akamai.edgegrid.signer.ClientCredential
