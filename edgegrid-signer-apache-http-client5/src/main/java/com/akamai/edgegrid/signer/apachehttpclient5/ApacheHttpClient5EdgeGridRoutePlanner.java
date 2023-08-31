2861f6fbfc2910151d40644215ff1a0759e27b44
Michal Mazur <mmazur@akamai.com>
2023-08-31 10:42:50 +0000

    DXE-2911 Https protocol issue fix

--- edgegrid-signer-apache-http-client5/src/main/java/com/akamai/edgegrid/signer/apachehttpclient5/ApacheHttpClient5EdgeGridRoutePlanner.java
+++ edgegrid-signer-apache-http-client5/src/main/java/com/akamai/edgegrid/signer/apachehttpclient5/ApacheHttpClient5EdgeGridRoutePlanner.java
@@ -1,34 +1,42 @@
 package com.akamai.edgegrid.signer.apachehttpclient5;
 
 import com.akamai.edgegrid.signer.ClientCredential;
-import org.apache.hc.client5.http.impl.routing.SystemDefaultRoutePlanner;
+import org.apache.hc.client5.http.HttpRoute;
+import org.apache.hc.client5.http.routing.HttpRoutePlanner;
+import org.apache.hc.core5.annotation.Contract;
+import org.apache.hc.core5.annotation.ThreadingBehavior;
+import org.apache.hc.core5.http.HttpException;
 import org.apache.hc.core5.http.HttpHost;
 import org.apache.hc.core5.http.protocol.HttpContext;
 
-import java.net.ProxySelector;
-
-public class ApacheHttpClient5EdgeGridRoutePlanner extends SystemDefaultRoutePlanner {
+@Contract(threading = ThreadingBehavior.STATELESS)
+public class ApacheHttpClient5EdgeGridRoutePlanner implements HttpRoutePlanner {
 
     private final ClientCredential clientCredential;
 
     public ApacheHttpClient5EdgeGridRoutePlanner(ClientCredential clientCredential) {
-        super(ProxySelector.getDefault());
         this.clientCredential = clientCredential;
     }
 
     @Override
-    protected HttpHost determineProxy(HttpHost target, HttpContext context) {
+    public HttpRoute determineRoute(HttpHost target, HttpContext context) throws HttpException {
         var hostname = clientCredential.getHost();
-        int port = -1;
+        int port = 443;
         final int pos = hostname.lastIndexOf(":");
         if (pos > 0) {
             try {
                 port = Integer.parseInt(hostname.substring(pos + 1));
+                if (port <= 0 || port > 65535) {
+                    throw new NumberFormatException();
+                }
             } catch (NumberFormatException ex) {
                 throw new IllegalArgumentException("Host contains invalid port number: " + hostname);
             }
             hostname = hostname.substring(0, pos);
         }
-        return new HttpHost("https", hostname, port);
+        HttpHost host = new HttpHost("https", hostname, port);
+        return new HttpRoute(host, null, true);
     }
 }
+
+
